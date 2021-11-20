28204eb9e2abaf784b77d6fabf9f3e131837d762
Jacques Etienne <santiagondiaye1@gmail.com>
2021-11-20 13:23:49 +0000

    Fixing the mini navbar on mobile version, stable but need enhancements

--- main.js
+++ main.js
@@ -33,39 +33,22 @@ miniLgVersionThree.addEventListener("change", versionThree);
 miniLgVersionTwo.addEventListener("change", versionTwo);
 miniLgVersionThree.addEventListener("change", versionThree);
 
-
-// window.addEventListener("scroll", function(){
-//     console.log(window.scrollY);
-//     var bougerHauteur = window.scrollY;
-// });
-
-// var bol;
-// var bougerHauteur = window.scrollY;
-// var parcours = document.querySelectorAll(".parcours-section");
-// for(let i = 0; i < parcours.length; i++){
-//     bol = parcours[i].getBoundingClientRect();
-//     console.log(bol);
-//     if (bougerHauteur - bol.y != 0) {
-//         console.log("Nous sommes sur la partie formation");
-//         console.log(bougerHauteur - bol.y)
-//     }
-// }
-
-// window.addEventListener("scroll", function (e) { 
-//     e.
-// });
-
 var blockParcoursTaille;
 var blockPublicTaille;
 var blockPartenariatTaille;
 var blockParcoursBtn;
 var blockPublicBtn;
+var parcoursVerticalMenuScroll;
+var publicVerticalMenuScroll;
+var verticalValue = 500;
 
 var parcours = document.querySelector(".parcours-section");
 var public = document.querySelector(".public-section");
 var partenariat = document.querySelector(".partenariat-section");
 var parcoursBtn = document.querySelector(".parcours-see-more-button");
 var publicBtn = document.querySelector(".public-see-more-button");
+var parcoursVerticalMenu = document.querySelector(".scope-parcours");
+var publicVerticalMenu = document.querySelector(".scope-public");
 
 blockParcoursTaille = parcours.getBoundingClientRect();
 blockPublicTaille = public.getBoundingClientRect();
@@ -78,24 +61,47 @@ console.log(blockPublicTaille);
 console.log(blockPartenariatTaille);
 console.log(blockParcoursBtn);
 
-function removeBackgroundClasslist(section){
-    section.classList.remove("section-color-change");
+// function removeBackgroundClasslist(section){
+//     section.classList.remove("section-color-change");
+// }
+
+// function addBackgroundClasslist(section){
+//     section.classList.add("section-color-change");
+// }
+
+
+function verticalMenuTopScroll(topValue, section){
+    topValue = topValue + 100;
+    section.style.top = topValue + "px";
+    if (window.scrollY > Math.ceil(section.getBoundingClientRect().bottom)) {
+        topValue = topValue + 100;
+        section.style.top = topValue + "px";
+    }
 }
 
-function addBackgroundClasslist(section){
-    section.classList.add("section-color-change");
+function hideDisplay(verticalNav){
+    verticalNav.style.display = "none";
+}
+
+function showDisplay(verticalNav){
+    verticalNav.style.display = "block";
 }
 
 window.addEventListener("scroll", function(e){
-   console.log(window.scrollY);
 
    if(window.scrollY >= Math.ceil(blockParcoursTaille.top) && window.scrollY < Math.ceil(blockParcoursBtn.top)){
-       addBackgroundClasslist(parcours);
-       removeBackgroundClasslist(public);
+    //    addBackgroundClasslist(parcours);
+    //    removeBackgroundClasslist(public);
+       verticalMenuTopScroll(verticalValue, parcoursVerticalMenu);
+       showDisplay(parcoursVerticalMenu);
    }else if(window.scrollY >= Math.ceil(blockPublicTaille.top) && window.scrollY < Math.ceil(blockPublicBtn.top)){
-       removeBackgroundClasslist(parcours);
-       addBackgroundClasslist(public);
+    //    removeBackgroundClasslist(parcours);
+    //    addBackgroundClasslist(public);
+       hideDisplay(parcoursVerticalMenu);
+       verticalMenuTopScroll(verticalValue, publicVerticalMenu);
+       showDisplay(publicVerticalMenu);
    }else{
-       removeBackgroundClasslist(public);
+    //    removeBackgroundClasslist(public);
+       hideDisplay(publicVerticalMenu);
    }
 });
